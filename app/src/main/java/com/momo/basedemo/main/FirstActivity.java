d3a0aa739a82064d7e6996afb602f0764bb921e5
wangzhongli <411278871>
2019-04-13 18:39:44 +0800

    去掉Androidx

---
+++ app/src/main/java/com/momo/basedemo/main/FirstActivity.java
@@ -0,0 +1,14 @@
+package com.momo.basedemo.main;
+
+import android.os.Bundle;
+
+import com.momo.basedemo.R;
+import com.momo.basedemo.base.BaseActivity;
+
+public class FirstActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
+}
